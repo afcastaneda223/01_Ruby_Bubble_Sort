76c726ffc5af0182ef259ae6251bd514e1191fd7
a2josue@yahoo.com <a2josue@yahoo.com>
2020-04-08 14:34:37 -0400

    adding bubble sort by

--- bubble.rb
+++ bubble.rb
@@ -1,4 +1,4 @@
-puts "First case: Bubble Sort\n"
+puts "First case: Bubble Sort"
 
 def bubble_sort(arr)
     n = arr.length-2
@@ -17,5 +17,27 @@ def bubble_sort(arr)
 
 first_test = bubble_sort([4,3,78,2,0,2])
 print "#{first_test} \n"
+
+puts "Second case: Bubble Sort By"
+
+def bubble_sort_by(arr)
+  n = arr.length-2
+  swapped = true
+  while swapped do
+    swapped = false
+    for i in 0..n
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    swapped = true
+      end
+    end
+  end
+  arr
+end
+
+second_test = bubble_sort_by(["hi","hello","hey", "hi", "hello", "hey"]) do |left,right|
+                left.length - right.length
+              end
+print "#{second_test} \n"
 
 
