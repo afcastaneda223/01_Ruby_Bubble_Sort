c315f2083b3da369c96188e9d660d4d42840c767
afcastaneda223 <af.castaneda223@gmail.com>
2020-04-08 14:46:42 -0300

    update bubble.rb

--- bubble.rb
+++ bubble.rb
@@ -1,22 +1,4 @@
-puts "First case: Bubble Sort"
-
-# def bubble_sort(array)
-#     (0...array.length).each do |x|
-#         (x...array.length).each do |y|
-#             if array[x] >= array[y]
-#                 array[x], array[y] = array[y], array[x]
-#             end
-#         end
-#     end
-#     #return the array when sorting
-#     array
-# end
-
-# # First test
-
-# first_test = bubble_sort([4,3,78,2,0,2])
-# print "#{first_test} \n"
-
+puts "First case: Bubble Sort\n"
 
 def bubble_sort(arr)
     n = arr.length
